35294fcabc9e2d84ac3ce61f25c9b75b72880211
wh.kim <whkimKitronyx@kitronyx.com>
2024-08-20 16:13:06 +0900

    Added comments: read_kitronyx_csv sample code
     for better clarity and understanding of the snapshot and log data reading processes.

--- python/sample/read_kitronyx_csv/main.py
+++ python/sample/read_kitronyx_csv/main.py
@@ -1,14 +1,21 @@
 import ReadKitronyxCsv as klib
 
-
 if __name__ == "__main__":
-    # Read 1d snpashot data
-    (row,col,data) =  klib.ReadSnapshot1DimensionData(r"snapshot_data\20240227T170929_AdcData-1d.csv")
-    print("row: ",row,"col: ",col,"len: ",len(data))
-    print("data: ",data)
+    # Read 1-dimensional snapshot data from the specified CSV file
+    (row, col, data) = klib.ReadSnapshot1DimensionData(r"snapshot_data\20240227T170929_AdcData-1d.csv")
+    
+    # Display the number of rows, columns, and the length of the data array
+    print("row: ", row, "col: ", col, "len: ", len(data))
+    
+    # Display the actual data content from the snapshot file
+    print("data: ", data)
 
-    # Read 1d Log data
-    (row,col,data_dict) =  klib.ReadConvertedLogFile1DimensionData(r"converted_log_data\20240311T142511_AdcData-1d.csv")
-    print("row: ",row,"col: ",col,"len: ",len(data_dict))    
+    # Read 1-dimensional log data from the specified CSV file
+    (row, col, data_dict) = klib.ReadConvertedLogFile1DimensionData(r"converted_log_data\20240311T142511_AdcData-1d.csv")
+    
+    # Display the number of rows, columns, and the length of the data dictionary
+    print("row: ", row, "col: ", col, "len: ", len(data_dict))
+    
+    # Iterate through the log data dictionary and print each time value and its corresponding data
     for time_value, matrix_data in data_dict.items():
         print(f"Time value: {time_value}, Data: {matrix_data}")

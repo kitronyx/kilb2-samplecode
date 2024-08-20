8905d9a278f595b268d7a33c945584f61fdb3688
wh.kim <whkimKitronyx@kitronyx.com>
2024-08-20 15:12:03 +0900

    Added detailed comments: read_kitronyx_csv
    -. the sample MATLAB code for better readability and understanding of the data reading and display process.

--- matlab/sample/read_kitronyx_csv/main.m
+++ matlab/sample/read_kitronyx_csv/main.m
@@ -1,21 +1,25 @@
+clc;
+% Read 1-dimensional snapshot data from a CSV file.
+[row, col, snapshotData] = read_snapshot_1d_data('snapshot_data\20240227T170929_AdcData-1d.csv');
 
-% Read 1-dimensional snapshot data.
-[row, col, data] = read_snapshot_1d_data('snapshot_data\20240227T170929_AdcData-1d.csv');
-disp(['row: ', num2str(row), ', col: ', num2str(col), ', len: ', num2str(length(data))]);
+% Display the number of rows, columns, and the length of the data array.
+disp(['row: ', num2str(row), ', col: ', num2str(col), ', len: ', num2str(length(snapshotData))]);
+
+% Display the actual data content.
 disp('data:');
-disp(data);
+disp(snapshotData);
 
-% Read 1-dimensional log data.
-[row, col, times, data] = read_converted_logfile_1D_data('converted_log_data\20240227T170929_AdcData-1d.csv');
-disp(['row: ', num2str(row), ', col: ', num2str(col), ', len: ', num2str(length(data))]);
+% Read 1-dimensional log data from a CSV file.
+[row, col, times, logData] = read_converted_logfile_1D_data('converted_log_data\20240227T170929_AdcData-1d.csv');
 
-% Print all data in the arrays
+% Display the number of rows, columns, and the length of the data array.
+disp(['row: ', num2str(row), ', col: ', num2str(col), ', len: ', num2str(length(logData))]);
+
+% Print all data in the arrays by iterating through each time and corresponding data entry.
 disp('Data:');
 for i = 1:numel(times)
-    key = times{i};
-    value = data{i};
+    key = times{i};  % Get the current time value.
+    value = logData{i}; % Get the corresponding data value.
+    % Display the time (key) and associated data (value) as a comma-separated string.
     disp(['Key: ', num2str(key), ', Value: ', strjoin(value, ', ')]);
 end
-
-
-
